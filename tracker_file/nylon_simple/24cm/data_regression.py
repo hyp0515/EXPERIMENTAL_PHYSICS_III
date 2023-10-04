bc0ec2b70ad7b9753e7e8cf3d0d4fb251cb7a0ce
hyp0515 <b102030019@g-mail.nsysu.edu.tw>
2023-10-04 17:54:44 +0800

    Modified excel files, Regression, and Backup

---
+++ tracker_file/nylon_simple/24cm/data_regression.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+file_path = "nylon_24_simple_2.xlsx"
+
+df = pd.read_excel(file_path, engine='openpyxl')
+raw_data = np.array(df)
+raw_data[:, 5] = np.deg2rad(raw_data[:, 5])
+
+columns = ['t', 'x', 'y', 'r', 'v', 'theta', 'p', 'k']
+data = {col: raw_data[:, idx] for idx, col in enumerate(columns)}
+
+# print(df)
+# print(data['t'])
+r = np.mean(data['r'])
+x_mean = np.mean(data['x'])
+x_amp = np.max(data['x'])-x_mean
+theta_mean = np.mean(data['theta'])
+# print(theta_mean)
+# plt.plot(data['t'], data['x'])
+# plt.plot(data['t'], np.cos(data['theta'])*r)
+# plt.show()
+###############################################################################
+def second_to_index(time):
+    return np.searchsorted(data['t'], time)
+###############################################################################
+
+def sin_function(t, A, omega, c, d):   
+    return A*np.sin(omega*t+c)+d
+
+params, covariance = curve_fit(
+    sin_function, 
+    data['t'][second_to_index(50):second_to_index(100)], 
+    data['x'][second_to_index(50):second_to_index(100)], 
+    p0=[x_amp, 0.2*np.pi, 0, 0],
+)
+print(x_amp)
+print(params)
+
+plt.scatter(data['t'], data['x'], label = 'experiment', color = 'red')
+plt.plot(data['t'], sin_function(data['t'], *params), label = 'fitted result')
+plt.legend()
+plt.show()
---
+++ tracker_file/spring_simple/0cm/data_regression.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+file_path = "spring_0_simple_3.xlsx"
+
+df = pd.read_excel(file_path, engine='openpyxl')
+raw_data = np.array(df)
+raw_data[:, 5] = np.deg2rad(raw_data[:, 5])
+
+columns = ['t', 'x', 'y', 'r', 'v', 'theta', 'p', 'k']
+data = {col: raw_data[:, idx] for idx, col in enumerate(columns)}
+
+r = np.mean(data['r'])
+x_mean = np.mean(data['x'])
+x_amp = np.max(data['x'])-x_mean
+theta_mean = np.mean(data['theta'])
+
+# plt.plot(data['t'], data['x'])
+# plt.show()
+###############################################################################
+def second_to_index(time):
+    return np.searchsorted(data['t'], time)
+###############################################################################
+
+def sin_function(t, A, decay, omega, c, d):   
+    return A*np.exp(-decay*t)*np.sin(omega*t+c)+d
+
+params, covariance = curve_fit(
+    sin_function, 
+    data['t'], 
+    data['x'], 
+    p0=[x_amp, 0.1, 0.2*np.pi, 0, 0],
+)
+# print(x_amp)
+# print(params)
+
+plt.scatter(data['t'], data['x'], label = 'experiment', color = 'red')
+plt.plot(data['t'], sin_function(data['t'], *params), label = 'fitted result')
+plt.legend()
+plt.show()
