22ac88a9852bad8810ca1c77aa79065c0b24f08f
hyp0515 <b102030019@g-mail.nsysu.edu.tw>
2023-10-14 15:01:42 +0800

    Data regression

---
+++ tracker_file/coupled_pendulum/move_1_40cm/couple_1.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from scipy.optimize import curve_fit, minimize
+###############################################################################
+# Data 
+def extract_data_from_path(path):
+    df = pd.read_excel(path, engine='openpyxl')
+    raw_data = np.array(df)
+    raw_data[:, 5] = np.deg2rad(raw_data[:, 5])
+    columns = ['t', 'x', 'y', 'r', 'v', 'theta']
+    data = {col: raw_data[:, idx] for idx, col in enumerate(columns)}
+    return data
+data_L = extract_data_from_path("./tracker_file/coupled_pendulum/move_1_40cm/couple_1_L.xlsx")
+data_R = extract_data_from_path("./tracker_file/coupled_pendulum/move_1_40cm/couple_1_R.xlsx")
+# sync time
+new_t_L = np.linspace(0, 69, len(data_L['t']))  
+new_t_R = np.linspace(0, 69, len(data_R['t']))
+ 
+# plt.plot(new_t_L, data_L['x'], color = 'red')
+# plt.plot(new_t_R, data_R['x'], color = 'blue')
+# plt.show()
+###############################################################################
+# Convenient function for getting index of time data
+def second_to_index_L(time):
+    return np.searchsorted(new_t_L, time)
+def second_to_index_R(time):
+    return np.searchsorted(new_t_R, time)
+###############################################################################
+# Fitting x displacement to get the period of the pendulum
+def sin_function(t, A, decay, omega_1, omega_2, d, e, f):   
+    return A*np.exp(-decay*t)*np.cos(omega_1*t+d)*np.sin(omega_2*t+e)+f
+
+params_R, covariance_R = curve_fit(
+    sin_function, 
+    new_t_R, 
+    data_R['x'],
+    p0=[1, 0.01, np.pi/23.3, 2*np.pi/1.45, 0, 0, 0.009],
+)  
+params_L, covariance_L = curve_fit(
+    sin_function, 
+    new_t_L, 
+    data_L['x'],
+    p0=[1, 0.01, np.pi/23.3, 2*np.pi/1.45, 0, 0, 0.009],
+)  
+
+print(params_R)
+print(params_L)
+plt.scatter(new_t_R, data_R['x'], label = 'experiment')
+plt.plot(new_t_R, sin_function(new_t_R, *params_R), label = 'fitted result')
+plt.scatter(new_t_L, data_L['x'], label = 'experiment')
+plt.plot(new_t_L, sin_function(new_t_L, *params_L), label = 'fitted result')
+plt.legend()
+plt.title("Original data and fitted result")
+plt.xlabel(r"t")
+plt.ylabel(r"x")
+plt.show()
+###############################################################################
---
+++ tracker_file/coupled_pendulum/move_1_40cm/couple_2.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from scipy.optimize import curve_fit, minimize
+###############################################################################
+# Data 
+def extract_data_from_path(path):
+    df = pd.read_excel(path, engine='openpyxl')
+    raw_data = np.array(df)
+    raw_data[:, 5] = np.deg2rad(raw_data[:, 5])
+    columns = ['t', 'x', 'y', 'r', 'v', 'theta']
+    data = {col: raw_data[:, idx] for idx, col in enumerate(columns)}
+    return data
+data_L = extract_data_from_path("./tracker_file/coupled_pendulum/move_1_40cm/couple_2_L.xlsx")
+data_R = extract_data_from_path("./tracker_file/coupled_pendulum/move_1_40cm/couple_2_R.xlsx")
+# sync time
+new_t_L = np.linspace(0, 71, len(data_L['t']))  
+new_t_R = np.linspace(0, 71, len(data_R['t']))
+ 
+# plt.plot(new_t_L, data_L['x'], color = 'red')
+# plt.plot(new_t_R, data_R['x'], color = 'blue')
+# plt.show()
+###############################################################################
+# Convenient function for getting index of time data
+def second_to_index_L(time):
+    return np.searchsorted(new_t_L, time)
+def second_to_index_R(time):
+    return np.searchsorted(new_t_R, time)
+###############################################################################
+# Fitting x displacement to get the period of the pendulum
+def sin_function(t, A, decay, omega_1, omega_2, d, e, f):   
+    return A*np.exp(-decay*t)*np.cos(omega_1*t+d)*np.sin(omega_2*t+e)+f
+
+params_R, covariance_R = curve_fit(
+    sin_function, 
+    new_t_R, 
+    data_R['x'],
+    p0=[-0.128, 0.00461, np.pi/25.666, 2*np.pi/1.52, 0, 0, 0.009],
+)  
+params_L, covariance_L = curve_fit(
+    sin_function, 
+    new_t_L, 
+    data_L['x'],
+    p0=[0.128, 0.00461, np.pi/25.666, 2*np.pi/1.52, 0, 0, 0.009],
+)  
+
+print(params_R)
+print(params_L)
+plt.scatter(new_t_R, data_R['x'], label = 'experiment')
+plt.plot(new_t_R, sin_function(new_t_R, *params_R), label = 'fitted result')
+plt.scatter(new_t_L, data_L['x'], label = 'experiment')
+plt.plot(new_t_L, sin_function(new_t_L, *params_L), label = 'fitted result')
+plt.legend()
+plt.title("Original data and fitted result")
+plt.xlabel(r"t")
+plt.ylabel(r"x")
+plt.show()
+###############################################################################
